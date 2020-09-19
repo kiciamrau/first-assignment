19bbba1b1628c980ce88989d8a5a22c408d6c62a
kiciamrau <62901617+kiciamrau@users.noreply.github.com>
2020-09-19 16:46:05 +0200

    Delete zestaw1 zad1.py

--- zestaw1 zad1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("train.tsv", delimiter='\t',
-                 names=["Cost", "NumberOfRooms", "Area", "Floor", "Address", "Description"])
-with open('out0.csv', 'w') as csvfile:
-    csvfile.write(str(df.Cost.mean().round(0)))
